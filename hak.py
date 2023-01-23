7065ea429db32db8e0319b0ab7cce8de614a853d
Seo <kirin2211@naver.com>
2023-01-23 22:18:44 +0900

    hak crew discord-bot

--- hak.py
+++ hak.py
@@ -18,7 +18,7 @@ from datetime import date
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="~", intents=intents)
 buttons = ButtonsClient(bot)
-token = "MTA2NDEyODQ3NzUwODQwNzQyNg.GzaE9j.O3nYg9GXXvtx8ua7PEEkiYNeIKWlJcvYY221_M"
+token = "MTA2NDEyODQ3NzUwODQwNzQyNg.GLfVLT.Ad84oSeen9lOYMCsBP5u_S_4Ijkn1NHf2QeCrY"
 
 
 @bot.event
